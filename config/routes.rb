b04b7090a016a9de11de74f22b9e0cdc36619c63
Nashe Osorio <nasheog2@gmail.com>
2022-10-04 13:15:18 -0500

    Index de materiales con diseño

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   #root "articles#index"
 
   resources :materials
-  #root "dashboard#index"
   #get 'material/index'
   #To Indica el controlador y la acción (index) que va a dar respuesta a esta petición (recurso bienvenida)
   #nombre del controlador se separa de la acción utilizando un gato
