d4b7b143f6d969c238490fe21f56ead79d19ff00
Nashe Osorio <nasheog2@gmail.com>
2022-10-04 21:24:26 -0500

    Se agrego un espacio entre botones y formulario de editar material

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   #root "articles#index"
 
-  resources :materials
   #get 'material/index'
   #To Indica el controlador y la acción (index) que va a dar respuesta a esta petición (recurso bienvenida)
   #nombre del controlador se separa de la acción utilizando un gato
   get "/bienvenida", to: "dashboard#index"
-  
+  resources :materials
+    
 end
