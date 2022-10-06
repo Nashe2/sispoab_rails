ae523dde15ab9c5e8ba0ec6dbc7e661955b9ba3f
Nashe Osorio <nasheog2@gmail.com>
2022-10-06 01:59:42 -0500

    Actualizacion del obmodelo rson y su migracion

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -65,6 +65,6 @@ class PeopleController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def person_params
-      params.require(:person).permit(:name, :first_name, :last_name, :phone, :sex)
+      params.require(:person).permit(:name, :first_name, :last_name, :phone, :gender)
     end
 end
--- db/migrate/20221006051345_create_people.rb
+++ db/migrate/20221006051345_create_people.rb
@@ -5,7 +5,7 @@ class CreatePeople < ActiveRecord::Migration[7.0]
       t.string :first_name
       t.string :last_name
       t.string :phone
-      t.boolean :is_sex
+      t.string :gender
 
       t.timestamps
     end
