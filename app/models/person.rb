aef92cc675c8a6e9d0ebda6bfb79c48fd4edad85
Nashe Osorio <nasheog2@gmail.com>
2022-10-06 02:22:58 -0500

    Validacion del modelo person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ApplicationRecord
+    validates :name, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence:true
+    validates :phone, presence: true
+    validates :gender, presence: true
+
 end
