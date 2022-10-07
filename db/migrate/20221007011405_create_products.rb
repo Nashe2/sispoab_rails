5405bf5dbedbbf75e199c27a0bd669dea4186927
Nashe Osorio <nasheog2@gmail.com>
2022-10-06 21:24:57 -0500

    Actualizacion y migracion del modelo producto

--- db/migrate/20221007011405_create_products.rb
+++ db/migrate/20221007011405_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :desription
+      t.string :description
       t.integer :quantity
 
       t.timestamps
